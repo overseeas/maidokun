39b80e736fcf11ba4e6a5bd73e499311c743eed0
Yang <overseeas@gmail.com>
2024-10-31 17:56:27 +0900

    Adding multiple lines were hard and slow. Should add item select window first

--- rakuten/urls.py
+++ rakuten/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', views.index, name="index"),
     path("update/", views.update, name="update"),
     path("export/", export.all_data_for_vlookup, name="export"),
+    path("testexport", export.item_and_sku, name="testexport"),
     path('<str:manage_number>/', views.detail, name="detail"),
 ]
